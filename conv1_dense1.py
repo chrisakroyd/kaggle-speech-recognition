770e5390343337c6a7bdf5fb51cc8c03ede0c367
Christopher Akroyd <chris@chrisakroyd.com>
2017-12-10 18:30:58 +0000

    Minor refactoring to remove a need for a static shape and added checkpointing.

--- conv1_dense1.py
+++ conv1_dense1.py
@@ -3,20 +3,27 @@ import time
 from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, Dense, Dropout, Flatten
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.callbacks import TensorBoard
+from keras.callbacks import TensorBoard, ModelCheckpoint
 
 from load_data import load_data
 from results import write_results
 
-from spectogram_generator import batch_generator
+from spectogram_generator import batch_generator, log_spectograms
 
 (x_train, y_train), (x_val, y_val), label_binarizer = load_data()
 
-WRITE_RESULTS = False
+WRITE_RESULTS = True
 # HPARAMS
 BATCH_SIZE = 32
-EPOCHS = 3
+EPOCHS = 5
 NUM_CLASSES = 12
+
+
+def get_data_shape():
+    wav_path = x_train[0]
+    spec = log_spectograms([wav_path])
+    print(spec[0].shape)
+    return spec[0].shape
 
 
 def get_model(shape):
@@ -38,7 +45,7 @@ def get_model(shape):
     return model
 
 
-shape = (129, 124, 1)
+shape = get_data_shape()
 model = get_model(shape)
 model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 
@@ -46,6 +53,7 @@ model.summary()
 # create training and test data.
 
 tensorboard = TensorBoard(log_dir='./logs/{}'.format(time.time()), batch_size=BATCH_SIZE)
+checkpoint = ModelCheckpoint('./test.hdf5', monitor='val_loss')
 
 train_gen = batch_generator(x_train.values, y_train, batch_size=BATCH_SIZE)
 valid_gen = batch_generator(x_val.values, y_val, batch_size=BATCH_SIZE, shuffle=False)
@@ -56,7 +64,7 @@ model.fit_generator(
     steps_per_epoch=x_train.shape[0] // BATCH_SIZE,
     validation_data=valid_gen,
     validation_steps=x_val.shape[0] // BATCH_SIZE,
-    callbacks=[tensorboard])
+    callbacks=[tensorboard, checkpoint])
 
 if WRITE_RESULTS:
     write_results(model, label_binarizer)
